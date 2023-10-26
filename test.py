467cbf98784c86cf3d900d00224279346ddd2122
Ailson Cardoso <ailsonpmp@gmail.com>
2023-10-26 01:34:07 +0000

    fixing set time branch

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Caio Delgado")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - Ailson Cardoso Diniz")
 
 
 if __name__ == "__main__":
